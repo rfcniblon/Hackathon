4f4626b04215b0f1bd9daf74132c13b2357af72d
rfcniblon <lambinet.nicolas@orange.fr>
2019-12-18 16:27:44 +0100

    encore un commit de M.... :)

---
+++ front/src/component/Resultat/Resultat.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../Resultat/Resultat.css';
+
+class ResultatInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      console.log(this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+            <input type="text" className="input_result" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" className="bouton_result" value="Envoyer" />
+        </form>
+      );
+    }
+  }
+
+export default ResultatInput;
