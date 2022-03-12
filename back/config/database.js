50f379cf53935967c6207d7dc5105f65ff2f47d9
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2022-03-12 01:56:50 +0100

    Delete back directory

--- back/config/database.js
+++
@@ -1,8 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  'localhost', // adresse du serveur
-user :  'root', // le nom d'utilisateur
-password :  'mot_de_pass', // le mot de passe
-database :  'hackathon2', // le nom de la base de données
-});
-module.exports = connection;
